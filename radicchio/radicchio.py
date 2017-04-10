61820f281b018be7952f942e7e70d68c7144f109
Marco Chiappetta <chpmrc@gmail.com>
2017-04-10 11:52:04 +0300

    Dummy implementation of SET

--- radicchio/radicchio.py
+++ radicchio/radicchio.py
@@ -2,7 +2,21 @@
 
 class Radicchio(object):
 
+    ERROR_STATUS = 'error'
+    OK_STATUS = 'ok'
+
     def handle(self, command, args={}):
+        status = self.OK_STATUS
+        result = None
+        try:
+            fn = getattr(self, command.lower())
+            result = fn(**args)
+        except AttributeError:
+            status = self.ERROR_STATUS
         return {
-            'status': 'ok'
+            'status': status,
+            'result': result
         }
+
+    def set(self, key, value):
+        return 'hello'
