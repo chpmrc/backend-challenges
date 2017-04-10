c2b78ecd8e8d8a7c0a175bd6653809b365755bb8
Marco Chiappetta <chpmrc@gmail.com>
2017-04-10 11:48:10 +0300

    Boilerplate

---
+++ radicchio/radicchio.py
@@ -0,0 +1,8 @@
+
+
+class Radicchio(object):
+
+    def handle(self, command, args={}):
+        return {
+            'status': 'ok'
+        }
---
+++ radicchio/test.py
@@ -0,0 +1,23 @@
+import unittest
+from radicchio import Radicchio
+
+
+class TestRadicchio(unittest.TestCase):
+
+    def setUp(self):
+        self.r = Radicchio()
+
+    def test_set(self):
+        payload = {
+            'command': 'SET',
+            'args': {
+                'key': 'a',
+                'value': 'b'
+            }
+        }
+        response = self.r.handle(**payload)
+        assert response['status'] == 'ok'
+
+
+if __name__ == '__main__':
+    unittest.main()
